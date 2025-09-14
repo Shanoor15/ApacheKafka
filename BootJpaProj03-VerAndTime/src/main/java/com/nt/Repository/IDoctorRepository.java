054d0c7c3ac1a03d3d661bf5e12e9e07a06684f6
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-14 16:59:04 +0530

    Delete BootJpaProj03-VerAndTime directory

--- BootJpaProj03-VerAndTime/src/main/java/com/nt/Repository/IDoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nt.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.Entity.Doctor;
-
-public interface IDoctorRepository extends JpaRepository<Doctor, Integer> {
-
-}
